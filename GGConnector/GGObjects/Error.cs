f27c399b62a26df9f0be06c89ce6a7d29c686f5f
Norgat <norg113@gmail.com>
2015-05-01 18:37:44 +0300

    Добавлены классы для парсинга сообщений чата и ошибок.

---
+++ GGConnector/GGObjects/Error.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace GGConnector.GGObjects {
+    [DataContract]
+    class Error {
+        [DataMember(Name = "channel_id")]
+        public int channel_id { get; set; }
+
+        [DataMember(Name = "error_num")]
+        public int error_num { get; set; }
+
+        [DataMember(Name = "errorMsg")]
+        public string errorMsg { get; set; }
+    }
+}
---
+++ GGConnector/GGObjects/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace GGConnector.GGObjects {
+    [DataContract]
+    class Message {
+        [DataMember(Name = "user_id")]
+        public int user_id { get; set; }
+
+        [DataMember(Name = "user_name")]
+        public string user_name { get; set; }
+
+        [DataMember(Name = "user_group")]
+        public int user_group { get; set; }
+
+        [DataMember(Name = "message_id")]
+        public int message_id { get; set; }
+
+        [DataMember(Name = "timestamp")]
+        public string timestamp { get; set; }
+
+        [DataMember(Name = "text")]
+        public string text { get; set; }
+    }
+}
