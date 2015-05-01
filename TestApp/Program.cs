675241bc724e89bcf506c95b11f60a28371578cc
Norgat <norg113@gmail.com>
2015-05-01 19:50:46 +0300

    Добавлен пример получения channel_id по нику стримера.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -10,53 +10,36 @@ using GGConnector.GGObjects;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
-
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace TestApp {
     class Program {
         static void Main(string[] args) {
-            using (var gg = new GG()) {
-                gg.Connect();
-                gg.GetChannelsList(0, 2);
+            //using (var gg = new GG()) {
+            //    gg.Connect();
+            //    gg.GetChannelsList(0, 2);
 
-                gg.GetUsersList(6147);
-
-                Console.ReadKey(true);
-                Console.WriteLine("CLOSE CONNECTION");
-            }
-
-            //var reqData = new ChannelsListData { start = 0, cout = 2 };
-            //var req = new ChannelsListRequest { type = "get_channels_list", data = reqData };
-
-            
-
-            //using (var ws = new WebSocket("ws://chat.goodgame.ru:8081/chat/websocket")) {
-            //    ws.OnMessage += (sender, e) => {
-            //        Console.WriteLine("MSG: {0}", e.Data);
-            //    };
-
-            //    ws.OnError += (sender, e) => {
-            //        Console.WriteLine("ERROR: {0}", e.Message);
-            //    };
-
-            //    ws.Connect();
-
-            //    var m = "{'type':'get_channels_list','data':{'start':0,'count':2}}";
-
-            //    using (var ms = new MemoryStream()) {
-            //        var ser = new DataContractJsonSerializer(typeof(ChannelsListRequest));
-            //        ser.WriteObject(ms, req);
-
-            //        ms.Position = 0;
-            //        var reader = new StreamReader(ms);
-            //        var message = reader.ReadToEnd();
-            //        Console.WriteLine("SEND: {0}", message);
-
-            //        ws.Send(message);
-            //    }
+            //    gg.GetUsersList(6147);
 
             //    Console.ReadKey(true);
+            //    Console.WriteLine("CLOSE CONNECTION");
             //}
+
+            var req = (HttpWebRequest)WebRequest.Create("http://goodgame.ru/api/getchannelstatus?id=SoVa&fmt=json");
+            var res = req.GetResponse();
+
+            using (var stream = new StreamReader(res.GetResponseStream())) {
+                var json = stream.ReadToEnd();
+                Console.WriteLine("JSON: {0}", json);
+
+                var regex = new Regex("^{\"[0-9]+\":");
+                var match = regex.Match(json);
+                var source = match.Value.Replace("{", "").Replace("\"", "").Replace(":","");
+
+                Console.WriteLine("RESULT ID: {0}", int.Parse(source));
+
+            }
 
             Console.ReadKey(true);
         }
