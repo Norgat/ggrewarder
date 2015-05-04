e22083e35a196a5f3f39c1970bb88c1781f027a3
Norgat <norg113@gmail.com>
2015-05-04 16:59:48 +0300

    Начата работа над графическим приложением.

--- Rewarder/MainWindow.xaml.cs
+++ Rewarder/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using GGConnector;
+
 namespace Rewarder {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -20,6 +22,17 @@ namespace Rewarder {
     public partial class MainWindow: Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void Button_Connect(object sender, RoutedEventArgs e) {
+            var streamer = StreamerNameInput.Text;
+
+            try {
+                var id = GG.GetChannelId(streamer);
+                MessageBox.Show(string.Format("Stream id: {0}", id));
+            } catch (Exception) {
+                MessageBox.Show("Не удалось получить Id стрима. Проверьте введённый ник стримера.");                
+            }
         }
     }
 }
